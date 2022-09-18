76ed3c906abe5f19e6c1222d93fdcff9b80a711e
Kristaps-M <kristapsmitins@gmail.com>
2022-09-18 23:23:07 +0300

    update test 02

--- sql-shopify/src/queries/select.ts
+++ sql-shopify/src/queries/select.ts
@@ -1,28 +1,41 @@
+import { APPS_CATEGORIES, CATEGORIES, REVIEWS } from "../shopify-table-names";
+
 export const selectCount = (table: string): string => {
-  throw new Error(`todo`);
+  return `SELECT COUNT (*) as c from ${table}`;
 };
 
 export const selectRowById = (id: number, table: string): string => {
-  throw new Error(`todo`);
+  return `SELECT ${id} FROM ${table}`;
 };
 
 export const selectCategoryByTitle = (title: string): string => {
-  throw new Error(`todo`);
+  return `SELECT ${title} FROM ${CATEGORIES}`;
 };
 
 export const selectAppCategoriesByAppId = (appId: number): string => {
-  throw new Error(`todo`);
+  return `SELECT * FROM ${APPS_CATEGORIES}
+  WHERE app_id = ${appId}`;
 };
 
-export const selectUnigueRowCount = (tableName: string, columnName: string): string => {
-  throw new Error(`todo`);
+export const selectUnigueRowCount = (
+  tableName: string,
+  columnName: string
+): string => {
+  return `SELECT ${columnName} FROM ${tableName}`;
 };
 
-export const selectReviewByAppIdAuthor = (appId: number, author: string): string => {
-  throw new Error(`todo`);
+export const selectReviewByAppIdAuthor = (
+  appId: number,
+  author: string
+): string => {
+  return `SELECT * FROM ${REVIEWS}
+  WHERE app_id = ${appId} AND
+  author = ${author}`;
 };
 
-export const selectColumnFromTable = (columnName: string, tableName: string): string => {
-  throw new Error(`todo`);
+export const selectColumnFromTable = (
+  columnName: string,
+  tableName: string
+): string => {
+  return `SELECT ${columnName} FROM ${tableName}`;
 };
-
--- sql-shopify/test/02-insert-data.test.ts
+++ sql-shopify/test/02-insert-data.test.ts
@@ -1,24 +1,43 @@
 import _ from "lodash";
 import { Database } from "../src/database";
 import { ShopifyCsvLoader } from "../src/data/shopify-csv-loader";
-import { App, Category, AppCategory, KeyBenefit, Review, PricingPlan, PricingPlanPrice } from "../src/data/shopify-types";
-import { APPS, CATEGORIES, KEY_BENEFITS, PRICING_PLANS, REVIEWS, APPS_CATEGORIES, APPS_PRICING_PLANS } from "../src/shopify-table-names";
+import {
+  App,
+  Category,
+  AppCategory,
+  KeyBenefit,
+  Review,
+  PricingPlan,
+  PricingPlanPrice,
+} from "../src/data/shopify-types";
+import {
+  APPS,
+  CATEGORIES,
+  KEY_BENEFITS,
+  PRICING_PLANS,
+  REVIEWS,
+  APPS_CATEGORIES,
+  APPS_PRICING_PLANS,
+} from "../src/shopify-table-names";
 import { escape } from "../src/utils";
 import { minutes, seconds } from "./utils";
-import { 
-    selectCount, 
-    selectRowById, 
-    selectCategoryByTitle, 
-    selectAppCategoriesByAppId, 
-    selectUnigueRowCount } from "../src/queries/select";
+import {
+  selectCount,
+  selectRowById,
+  selectCategoryByTitle,
+  selectAppCategoriesByAppId,
+  selectUnigueRowCount,
+} from "../src/queries/select";
 import { resolve } from "path";
 
 const pricingPlansDir = resolve(__dirname, "../_data/pricing_plans.csv");
 
 const insertApps = (apps: App[]) => {
-    return (
-        `todo` + 
-        apps.map(app => `('${app.url}',
+  return (
+    `insert into ${APPS} (title) values` +
+    apps
+      .map(
+        (app) => `('${app.url}',
             '${escape(app.title)}',
             '${escape(app.tagline)}',
             '${escape(app.developer)}',
@@ -27,48 +46,59 @@ const insertApps = (apps: App[]) => {
             ${app.rating},
             ${app.reviewsCount},
             '${escape(app.description)}',
-            '${escape(app.pricingHint)}')`).join(",")
-    );
+            '${escape(app.pricingHint)}')`
+      )
+      .join(",")
+  );
 };
 
 const insertCategories = (categories: Category[]) => {
-    return (
-        `todo` + 
-        categories.map(category => `('${category.title}')`).join(",")
-    );
+  return (
+    `INSERT INTO ${CATEGORIES} (title) VALUES` +
+    categories.map((category) => `('${category.title}')`).join(",")
+  );
 };
 
 const insertAppCategories = (appCategories: AppCategory[]) => {
-    return (
-        `todo` + 
-        appCategories.map(appCategory => 
-            `('${appCategory.shopifyAppId}',
-            '${appCategory.categoryId}')`).join(",")
-    );
+  return (
+    `INSERT INTO ${APPS_CATEGORIES} (app_title) VALUES` +
+    appCategories
+      .map(
+        (appCategory) =>
+          `('${appCategory.shopifyAppId}',
+            '${appCategory.categoryId}')`
+      )
+      .join(",")
+  );
 };
 
 const insertKeyBenefits = (keyBenefits: KeyBenefit[]) => {
-    return (
-        `todo` + 
-        keyBenefits.map(keyBenefit => 
-            `('${keyBenefit.shopifyAppId}',
+  return (
+    `INSERT INTO ${KEY_BENEFITS} (description) VALUES` +
+    keyBenefits
+      .map(
+        (keyBenefit) =>
+          `('${keyBenefit.shopifyAppId}',
             '${escape(keyBenefit.title)}',
-            '${escape(keyBenefit.description)}')`).join(",")
-    );
+            '${escape(keyBenefit.description)}')`
+      )
+      .join(",")
+  );
 };
 
 const insertPricingPlans = (pricingPlans: string[]) => {
-    return (
-        `todo` + 
-        pricingPlans.map(pricingPlan => 
-            `('${pricingPlan}')`).join(",")
-    );
+  return (
+    `INSERT INTO ${PRICING_PLANS} (price) VALUES` +
+    pricingPlans.map((pricingPlan) => `('${pricingPlan}')`).join(",")
+  );
 };
 
 const insertReviews = (reviews: Review[]) => {
-    return (
-        `todo`+ 
-            reviews.map(review => `(
+  return (
+    `INSERT INTO ${REVIEWS} (author) VALUES` +
+    reviews
+      .map(
+        (review) => `(
                 ${review.shopifyAppId},
                 '${escape(review.author)}',
                 '${escape(review.body)}',
@@ -76,139 +106,190 @@ const insertReviews = (reviews: Review[]) => {
                 ${review.helpfulCount},
                 '${review.dateCreated}',
                 '${escape(review.developerReply)}',
-                '${review.developerReplyDate}')`).join(",")
-    );
+                '${review.developerReplyDate}')`
+      )
+      .join(",")
+  );
 };
 
-const insertAppPricingPlans = (pricingPlans: PricingPlan[], prices: PricingPlanPrice[]) => {
-    return (
-        `todo` + 
-        pricingPlans.map(pricingPlan => 
-            `('${pricingPlan.shopifyAppId}', '${prices.find(it => it.price === pricingPlan.price)!.id}')
-            `).join(",")
-    );
+const insertAppPricingPlans = (
+  pricingPlans: PricingPlan[],
+  prices: PricingPlanPrice[]
+) => {
+  return (
+    `INSERT INTO ${APPS_PRICING_PLANS} (price) VALUES` +
+    pricingPlans
+      .map(
+        (pricingPlan) =>
+          `('${pricingPlan.shopifyAppId}', '${
+            prices.find((it) => it.price === pricingPlan.price)!.id
+          }')
+            `
+      )
+      .join(",")
+  );
 };
 
 describe("Insert Data", () => {
-    let db: Database;
+  let db: Database;
 
-    beforeAll(async () => {
-        db = await Database.fromExisting("01", "02");
-        await ShopifyCsvLoader.load();
-    }, seconds(30));
+  beforeAll(async () => {
+    db = await Database.fromExisting("01", "02");
+    await ShopifyCsvLoader.load();
+  }, seconds(30));
 
-    it("should insert apps data", async done => {
-        const apps = await ShopifyCsvLoader.apps();
-        const chunks = _.chunk(apps, 500);
+  it(
+    "should insert apps data",
+    async (done) => {
+      const apps = await ShopifyCsvLoader.apps();
+      const chunks = _.chunk(apps, 500);
 
-        for (const ch of chunks){
-            await db.insert(insertApps(ch));
-        }
-        const count = await db.selectSingleRow(selectCount(APPS));
-        expect(count.c).toBe(2831);
+      for (const ch of chunks) {
+        await db.insert(insertApps(ch));
+      }
+      const count = await db.selectSingleRow(selectCount(APPS));
+      expect(count.c).toBe(2831);
 
-        const row = await db.selectSingleRow(selectRowById(1000, APPS));
-        expect(row.title).toEqual("Yottie ‑ YouTube Video App");
+      const row = await db.selectSingleRow(selectRowById(1000, APPS));
+      expect(row.title).toEqual("Yottie ‑ YouTube Video App");
 
-        done();
+      done();
     },
-    minutes(1));
+    minutes(1)
+  );
 
-    it("should insert categories data", async done => {
-        const categories = await ShopifyCsvLoader.categories();
-        await db.insert(insertCategories(categories));
+  it(
+    "should insert categories data",
+    async (done) => {
+      const categories = await ShopifyCsvLoader.categories();
+      await db.insert(insertCategories(categories));
 
-        const count = await db.selectSingleRow(selectCount(CATEGORIES));
-        expect(count.c).toBe(12);
+      const count = await db.selectSingleRow(selectCount(CATEGORIES));
+      expect(count.c).toBe(12);
 
-        const row = await db.selectSingleRow(selectRowById(5, CATEGORIES));
-        expect(row.title).toEqual("Customer support");
+      const row = await db.selectSingleRow(selectRowById(5, CATEGORIES));
+      expect(row.title).toEqual("Customer support");
 
-        const rowByTitle = await db.selectSingleRow(selectCategoryByTitle("Places to sell"));
-        expect(rowByTitle.id).toEqual(12);
+      const rowByTitle = await db.selectSingleRow(
+        selectCategoryByTitle("Places to sell")
+      );
+      expect(rowByTitle.id).toEqual(12);
 
-        done();
+      done();
     },
-    minutes(1));
+    minutes(1)
+  );
 
-    it("should insert app categories data", async done => {
-        const appCategories = await ShopifyCsvLoader.appCategories();
-        const chunks = _.chunk(appCategories, 500);
-        for (const ch of chunks){
-            await db.insert(insertAppCategories(ch));
-        }
+  it(
+    "should insert app categories data",
+    async (done) => {
+      const appCategories = await ShopifyCsvLoader.appCategories();
+      const chunks = _.chunk(appCategories, 500);
+      for (const ch of chunks) {
+        await db.insert(insertAppCategories(ch));
+      }
 
-        const count = await db.selectSingleRow(selectCount(APPS_CATEGORIES));
-        expect(count.c).toEqual(4155);
+      const count = await db.selectSingleRow(selectCount(APPS_CATEGORIES));
+      expect(count.c).toEqual(4155);
 
-        const appCategoriesRows = await db.selectMultipleRows(selectAppCategoriesByAppId(1056));
-        expect(appCategoriesRows).toEqual([
-            { app_title: "mmuze", category_id: 1, category_title: "Store design" },
-            { app_title: "mmuze", category_id: 2, category_title: "Sales and conversion optimization" },
-            { app_title: "mmuze", category_id: 3, category_title: "Marketing" },
-            { app_title: "mmuze", category_id: 5, category_title: "Customer support" }
-        ]);
-        done();
+      const appCategoriesRows = await db.selectMultipleRows(
+        selectAppCategoriesByAppId(1056)
+      );
+      expect(appCategoriesRows).toEqual([
+        { app_title: "mmuze", category_id: 1, category_title: "Store design" },
+        {
+          app_title: "mmuze",
+          category_id: 2,
+          category_title: "Sales and conversion optimization",
+        },
+        { app_title: "mmuze", category_id: 3, category_title: "Marketing" },
+        {
+          app_title: "mmuze",
+          category_id: 5,
+          category_title: "Customer support",
+        },
+      ]);
+      done();
     },
-    minutes(1));
+    minutes(1)
+  );
 
-    it("should insert key benefits data", async done => {
-        const keyBenefits = await ShopifyCsvLoader.keyBenefits();
-        await db.execute("PRAGMA foreign_keys = ON");
-        await db.insert(insertKeyBenefits(keyBenefits));
-    
-        const count = await db.selectSingleRow(selectCount(KEY_BENEFITS));
-        expect(count.c).toEqual(7446);
+  it(
+    "should insert key benefits data",
+    async (done) => {
+      const keyBenefits = await ShopifyCsvLoader.keyBenefits();
+      await db.execute("PRAGMA foreign_keys = ON");
+      await db.insert(insertKeyBenefits(keyBenefits));
 
-        const uniqueValues = await db.selectSingleRow(selectUnigueRowCount(KEY_BENEFITS, "description"));
-        expect(uniqueValues.c).toEqual(7281);
-        done();
+      const count = await db.selectSingleRow(selectCount(KEY_BENEFITS));
+      expect(count.c).toEqual(7446);
+
+      const uniqueValues = await db.selectSingleRow(
+        selectUnigueRowCount(KEY_BENEFITS, "description")
+      );
+      expect(uniqueValues.c).toEqual(7281);
+      done();
     },
-    minutes(1));
+    minutes(1)
+  );
 
-    it("should insert pricing plans", async done => {
-        const pricingPlans = await ShopifyCsvLoader.loadUniquePricingPlans(pricingPlansDir);
-        await db.insert(insertPricingPlans(pricingPlans));
+  it(
+    "should insert pricing plans",
+    async (done) => {
+      const pricingPlans = await ShopifyCsvLoader.loadUniquePricingPlans(
+        pricingPlansDir
+      );
+      await db.insert(insertPricingPlans(pricingPlans));
 
-        const count = await db.selectSingleRow(selectCount(PRICING_PLANS));
-        expect(count.c).toEqual(365);
+      const count = await db.selectSingleRow(selectCount(PRICING_PLANS));
+      expect(count.c).toEqual(365);
 
-        const row = await db.selectSingleRow(selectRowById(333, PRICING_PLANS));
-        expect(row.price).toEqual("$23.70/month");
-        done();
+      const row = await db.selectSingleRow(selectRowById(333, PRICING_PLANS));
+      expect(row.price).toEqual("$23.70/month");
+      done();
     },
-    minutes(1));
+    minutes(1)
+  );
 
-    it("should insert review data", async done => {
-        const reviews = await ShopifyCsvLoader.reviews();
-        const chunks = _.chunk(reviews, 500);
-        for (const ch of chunks){
-            await db.insert(insertReviews(ch));
-        }
+  it(
+    "should insert review data",
+    async (done) => {
+      const reviews = await ShopifyCsvLoader.reviews();
+      const chunks = _.chunk(reviews, 500);
+      for (const ch of chunks) {
+        await db.insert(insertReviews(ch));
+      }
 
-        const count = await db.selectSingleRow(selectCount(REVIEWS));
-        expect(count.c).toEqual(291767);
+      const count = await db.selectSingleRow(selectCount(REVIEWS));
+      expect(count.c).toEqual(291767);
 
-        const uniqueAuthors = await db.selectSingleRow(selectUnigueRowCount(REVIEWS, "author"));
-        expect(uniqueAuthors.c).toEqual(191707);
+      const uniqueAuthors = await db.selectSingleRow(
+        selectUnigueRowCount(REVIEWS, "author")
+      );
+      expect(uniqueAuthors.c).toEqual(191707);
 
-        done();
+      done();
     },
     minutes(2)
-);
+  );
 
-    it("should insert apps pricing plans data", async done => {
-        const pricePlans = await ShopifyCsvLoader.pricingPlans();
-        const prices = (await db.selectMultipleRows(`todo`)) as PricingPlanPrice[];
-        
-        const chunks = _.chunk(pricePlans, 500);
-        for (const ch of chunks){
-            await db.insert(insertAppPricingPlans(ch, prices));
-        }
+  it(
+    "should insert apps pricing plans data",
+    async (done) => {
+      const pricePlans = await ShopifyCsvLoader.pricingPlans();
+      const prices = (await db.selectMultipleRows(
+        `SELECT * FROM ${APPS}`
+      )) as PricingPlanPrice[];
 
-        const count = await db.selectSingleRow(selectCount(APPS_PRICING_PLANS));
-        expect(count.c).toEqual(4896);
-        done();
+      const chunks = _.chunk(pricePlans, 500);
+      for (const ch of chunks) {
+        await db.insert(insertAppPricingPlans(ch, prices));
+      }
+
+      const count = await db.selectSingleRow(selectCount(APPS_PRICING_PLANS));
+      expect(count.c).toEqual(4896);
+      done();
     },
-    minutes(1));
+    minutes(1)
+  );
 });
